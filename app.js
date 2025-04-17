34f71cb5a050c56d84c7a06c52f6f69d9bdbcd13
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2025-04-17 11:47:37 +0200

    add try catch block for weather api. add dynamic display of user location and weather

---
+++ app.js
@@ -0,0 +1,40 @@
+const locationCont = document.getElementById('location-cont')
+const weatherIconCont = document.getElementById('weather-icon-cont')
+locationCont.addEventListener('click', () => console.log("clicked"))
+
+
+navigator.geolocation.getCurrentPosition(async position => {
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    const apiKey = "686e4332ae1595567be7c5967ad6a159";
+
+    try {
+        // Reverse Geocoding - get city name
+        const geoRes = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=5&appid=${apiKey}`);
+        if (!geoRes.ok) {
+            throw new Error("City not found");
+        }
+        const geoData = await geoRes.json();
+        const name = geoData[0].name;
+        const state = geoData[0].state;
+        const country = geoData[0].country;
+       
+
+        // Weather Data
+        const weatherRes = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`);
+        if (!weatherRes.ok) {
+            throw new Error("Weather data not available");
+        }
+        const weatherData = await weatherRes.json();
+        const temp = Math.round(weatherData.main.temp);
+        const weatherDesc = weatherData.weather[0].description
+        const weatherIconUrl = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+    
+        
+
+        locationCont.innerHTML = `The temperature in <strong>${name}</strong>, ${state}, ${country} is `;
+        weatherIconCont.innerHTML = `${temp}°C.<img src='${weatherIconUrl}'/> ${weatherDesc}!`
+    } catch (err) {
+        console.error(err);
+    }
+});
